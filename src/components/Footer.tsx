5a2fa557899b593ba8cbca29cf05eb19cc4f71c7
just-do-it-right <indeed@roelmacias.com>
2025-06-17 22:04:12 -0500

    Add project images

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -23,11 +23,10 @@ export const Footer = () => {
         vertical="center"
       >
         <Text variant="body-default-s" onBackground="neutral-strong">
-          <Text onBackground="neutral-weak">© {currentYear} /</Text>
-          <Text paddingX="4">{person.name}</Text>
+          <Text onBackground="neutral-weak">© {currentYear} / </Text>
           <Text onBackground="neutral-weak">
             {/* Usage of this template requires attribution. Please don't remove the link to Once UI. */}
-            / Powered by Vercel + Next.js +{" "}
+            Powered by Vercel + Next.js +{" "}
             <SmartLink
               href="https://once-ui.com/templates/magic-portfolio"
             >
